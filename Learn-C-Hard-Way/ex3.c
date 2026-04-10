2f39e818278e859be269d313cd2b09804795b1e9
HE YUXUAN <christopherhyx@outlook.com>
2026-04-10 19:53:38 +0800

    INIT Learn-C-Hard-Way

---
+++ Learn-C-Hard-Way/ex3.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    int age = 10;
+    int height = 72;
+
+    printf("I am %dyears old\n", age);
+    printf("I am %dinches high\n", height);
+
+    return 0;
+}
